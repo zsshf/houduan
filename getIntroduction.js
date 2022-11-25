80b743efc31628e77b8f589963d288a2693ef0e3
zsshf <2551717182@qq.com>
2022-11-25 17:20:41 +0800

    add getIntroduction.js

---
+++ getIntroduction.js
@@ -0,0 +1,25 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+let data=[]
+cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV }) // 使用当前云环境
+const db=cloud.database();
+// 云函数入口函数
+exports.main = async (event, context) => {
+    try{
+        return await db.collection('introduction').where({
+          title : event.title
+        })
+        .get({
+          success: function(res) {
+            console.log(res.result.data)
+            this.setData({
+              data:res.result.data
+            })
+            return data
+          }
+        })
+         
+    }catch (e) {
+        console.error(e)
+    } 
+}
